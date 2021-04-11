0d6a3a35998cdbe2cd2818f71c51b37770f6d276
autopy-lot action <autopy-lot@master>
2021-04-11 05:06:46 +0000

    Add/Update autopy-lot outputs

--- Unit_Test/Final_Model.py
+++ Unit_Test/Final_Model.py
@@ -100,15 +100,15 @@ df.info()
 # Some more information about the dataset
 
 
-#display(df.shape)
-#display(df.isnull().sum())
-#display(df.describe())
+display(df.shape)
+display(df.isnull().sum())
+display(df.describe())
 
 # +
 # Cheking for unique values
 
-#display(df['Liability-Assets Flag'].nunique())
-#display(df['Net Income Flag'].nunique())
+display(df['Liability-Assets Flag'].nunique())
+display(df['Net Income Flag'].nunique())
 
 # +
 # Dropping these 2 columnds
@@ -158,8 +158,8 @@ y = df['Bankrupt?']
 
 X.head()
 
-#display(X.shape)
-#display(y.shape)
+display(X.shape)
+display(y.shape)
 
 # !pip install category_encoders
 
@@ -247,8 +247,8 @@ X.columns = x_col
 display(X.head())
 '''
 
-#display(X_train.shape)
-#display(y_train.shape)
+display(X_train.shape)
+display(y_train.shape)
 
 count = 0
 for i in y_train:
@@ -400,7 +400,7 @@ for i in values.index:
 # -
 
 ctr = len(values)
-#print("Number of observations dropped = {}".format(ctr))
+print("Number of observations dropped = {}".format(ctr))
 
 # +
 # Modelling with balanced target 
@@ -436,8 +436,8 @@ over_sample=SMOTE()
 X_train_prepared, y_train=over_sample.fit_resample(X_train_prepared,y_train)
 # -
 
-#display(X_train_prepared.shape)
-#display(y_train.shape)
+display(X_train_prepared.shape)
+display(y_train.shape)
 
 plt.figure(figsize=(5,5))
 splot = sns.countplot(data = y_train, x = 'Bankrupt?', palette = 'Blues')
